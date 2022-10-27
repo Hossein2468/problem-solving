4be6ca20043265941c940779a15be8ee7e34fb6e
Hossein2468 <hosseinbaziepic@gmail.com>
2022-10-27 12:23:45 +0330

    nothing added

--- problems/P030/P030-Letters list.py
+++ problems/P030/P030-Letters list.py
@@ -1,2 +1,17 @@
-x = "I have no idea"
-print(x)
+n = int(input("Enter the number: "))
+letters_list = []
+words_list = []
+for m in range(n):
+    m = input("Enter the letters: ").split()
+    letters_list.append(m)
+o = " "
+while o != "END" :
+    o = input("Enter the words: ")
+    words_list.append(o)
+words_list.remove("END")
+
+def position(a):
+    from collections import namedtuple 
+    pos = namedtuple('position' , ['y' , 'x'])
+
+print(position(letters_list))
