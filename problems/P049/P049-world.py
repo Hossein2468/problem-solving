a3b25adc48bff2f2d6aaa7d294b8b90fae191edd
Hossein2468 <hosseinbaziepic@gmail.com>
2022-10-27 12:21:14 +0330

    nothing added

--- problems/P049/P049-world.py
+++ problems/P049/P049-world.py
@@ -1,2 +1,10 @@
-n = 'I have no idea'
-print(n)
+hw = input("Enter the 'W' and'H' : ").split()
+wh = list(map(lambda x : int(x) , hw))
+n = int(input("Enter the number of stars: "))
+stars = {} 
+for n1 in range(n) :
+    n2 = input("Enter the stars names: ").split()
+    stars.update({n2[0] : [int(n2[1]) , int(n2[2])]})
+my_position = input("Enter my Position: ").split()
+pos = list(map(lambda x : int(x) , my_position))
+target = str(input("Enter the star target: ")) 
