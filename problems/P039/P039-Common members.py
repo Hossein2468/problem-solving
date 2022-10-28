bf68363b4b2204b896b6312fffdf39c38a10cd7e
Hossein2468 <hosseinbaziepic@gmail.com>
2022-10-28 18:36:01 +0330

    added P039

--- problems/P039/P039-Common members.py
+++ problems/P039/P039-Common members.py
@@ -6,12 +6,12 @@ for m in range(n - 1):
     m = input("Enter the numbers list: ").split()
     o = list(map(lambda x : int(x) , m))
     a.append(o)
-for c in q :
+for c in set(q) :
     x = True 
     b = 0 
     while b < len(a) :
         d = a[b] 
-        if c not in d :
+        if c not in set(d) :
             x = False 
         b += 1 
     if x == True :
