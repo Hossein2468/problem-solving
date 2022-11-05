4eb01e7ded05eda813f6b27b99118a9a6fdb8445
Hossein2468 <hosseinbaziepic@gmail.com>
2022-11-05 18:16:08 +0330

    added P001 question

--- problems/P001/P001-even odd.py
+++ problems/P001/P001-even odd.py
@@ -1,5 +1,10 @@
-h = int(input("enter the number:"))
+question = str(input("Do you like to run with sample input? (y/n): "))
+if question == 'y' : 
+    import random 
+    h = random.randint(1 , 10) 
+if question == 'n' : 
+    h = int(input("enter the number:"))
 if h % 2 == 0 :
-    print ("number is even.")
+    print (f"{h} is even.")
 else:
-    print ("number is odd.")
+    print (f"{h} is odd.")
