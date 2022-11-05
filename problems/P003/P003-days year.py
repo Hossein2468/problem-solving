a0517a5a07fe441e3ee2448935c7246a77355dc0
Hossein2468 <hosseinbaziepic@gmail.com>
2022-11-05 19:36:49 +0330

    added P003 question

--- problems/P003/P003-days year.py
+++ problems/P003/P003-days year.py
@@ -1,5 +1,11 @@
-a = int(input("enter the first year: "))
-b = int(input("enter the second year: "))
+question = str(input("Do you like to run with sample input? (y/n): "))
+if question == 'y' : 
+    a = 1400 
+    b = 1401 
+if question == 'n' : 
+
+    a = int(input("enter the first year: "))
+    b = int(input("enter the second year: "))
 a = (a * 365)
 b = (b * 365)
 c = (a - b)
