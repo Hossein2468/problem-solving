ba0018b802a0684ce63b00a04011eef4631cc54e
Hossein2468 <hosseinbaziepic@gmail.com>
2022-11-05 19:40:27 +0330

    added P004 question

--- problems/P004/P004-numbers.py
+++ problems/P004/P004-numbers.py
@@ -1,3 +1,9 @@
-a = input("enter the number: ")
+question = str(input("Do you like to run with sample input? (y/n): "))
+if question == 'y' : 
+    import random 
+    a1 = random.randint(1 , 10)
+    a = str(a1) 
+if question == 'n' : 
+    a = input("enter the number: ")
 b = {"1" : "one" , "2" : "two" , "3" : "three" , "4" : "four" , "5" : "five" , "6" : "six" , "7" : "seven" , "8" : "eight" , "9" : "nine" , "10" : "ten"}
 print (b[a])
