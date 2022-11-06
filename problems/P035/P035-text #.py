bdd5e0b93b1ecfb4c304a944a7678ce10f8832ab
Hossein2468 <hosseinbaziepic@gmail.com>
2022-11-06 19:36:04 +0330

    added P035

--- problems/P035/P035-text #.py
+++ problems/P035/P035-text #.py
@@ -1,80 +1,42 @@
 sentences = []
-word = []
 words = []
 while True :
-    a = input("Enter the sentences: ").split()
-    if a == ["END"] :
+    a = input("Enter the sentences: ")
+    if a == "END" :
         break 
     sentences.append(a)
 b = 1
 while b < len (sentences) :
     c = sentences[0]
     d = sentences[b] 
-    for e in c :
-        for f in d :
-            if e == f : 
-                word.append(e)
-                words.append(f)
-                if b == 1 :
-                    words.append(e)
-            if e != f : 
-                g = 0 
-                while g < len(e):
-                    for h in range((g + 1) , (len(e) + 1)):
-                        i = e[g : h]
-                        if i == f :
-                            word.append(f)
-                            words.append(e)
-                            if b == 1 :
-                                words.append(e)
-                    g += 1
-            if e != f : 
-                g = 0 
-                while g < len(f):
-                    for h in range((g + 1) , (len(f) + 1)):
-                        i = f[g : h]
-                        if i == e :
-                            word.append(e)
-                            words.append(f)
-                            if b == 1 :
-                                words.append(e)
-                    g += 1
-    b += 1 
-
-lentghs = list(map(lambda x : len(x) , word))
-lentgh = set(word)
-l = lentghs.index(max(lentghs))
-m = word[l]
-trashes_words = [] 
-for le in lentgh :
-    if le != m : 
-           trashes_words.append(le)
-for gh in words :
-    if gh in trashes_words :
-        hg = words.count(gh) 
-        for gh1 in range(hg):
-            words.remove(gh)
+    c1 = 0
+    while c1 < len(c) : 
+        for c2 in range(c1 , len(c) + 1) : 
+            c3 = c[c1 : c2] 
+            d1 = 0 
+            while d1 < len(d) : 
+                for d2 in range(d1 , len(d) + 1) :
+                    d3 = d[d1 : d2] 
+                    if c3 == d3 and c3 != ' ' and c3 != '' and c[0] != ' ' and c3[-1] != ' ' : 
+                        words.append(c3) 
+                d1 += 1 
+        c1 += 1 
+    b += 1
+e = list(map(lambda x : len(x) , words))
+e1 = e.index(max(e)) 
+word = words[e1] 
 index = [] 
-wo = 0 
-while wo < len(sentences) :
-    the_word = words[wo] 
-    the_sentence = sentences[wo] 
-    jo = ' '.join(the_sentence)
-    index.append(jo.index(the_word))
-    wo += 1 
-max_index = max(index) 
-the_index = index.index(max_index)
-ow = 0 
-while ow < len(sentences) :
-    tword = words[ow] 
-    tsentence = sentences[ow]
-    tsentence1 = ' ' .join(tsentence)
-    if tsentence1.index(tword) != max_index :
-        abcd = max_index - tsentence1.index(tword) - 1
-        tsentence.insert(0 , '#' * abcd)
-        string_sentence = ' '.join(tsentence)
-        print(string_sentence)
-    if tsentence1.index(tword) == max_index :
-        string_sentence = ' '.join(tsentence)
-        print(string_sentence)
-    ow += 1 
+for i in sentences : 
+    index.append(i.index(word)) 
+i1 = max(index) 
+i2 = 0 
+while i2 < len(sentences) :
+    sentence = sentences[i2] 
+    if sentence.index(word) < i1 : 
+        u = i1 - sentence.index(word) - 1
+        o = sentence.split() 
+        o.insert(0 , '#' * u)
+        print(' '.join(o)) 
+    else : 
+        print(sentence)
+    i2 += 1
