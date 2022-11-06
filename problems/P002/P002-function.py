1cd48897405344d2ecffab4ff97469e00408052e
Hossein2468 <hosseinbaziepic@gmail.com>
2022-11-06 17:14:52 +0330

    added P002 question

--- problems/P002/P002-function.py
+++ problems/P002/P002-function.py
@@ -2,18 +2,20 @@ question = str(input("Do you like to run with sample input? (y/n): "))
 if question == 'y' : 
     a = 2 
     b = 3
+    print('a =' , a)
+    print('b =' , b) 
 if question == 'n' : 
     a = int(input("enter the first number:"))
     b = int(input("enter the second number:"))
-    def my_sum(x , y): 
-        print (x + y)
-    def my_subtract(x , y):
-        print (y - x) 
-    def my_multiply(x , y):
-        print (x * y) 
-    def my_divide(x , y):
-        print (y / x)
-    my_sum(a , b)
-    my_subtract(a , b)
-    my_multiply(a , b)
-    my_divide(a , b)
+def my_sum(x , y): 
+    return x + y
+def my_subtract(x , y):
+    return y - x 
+def my_multiply(x , y):
+    return x * y 
+def my_divide(x , y):
+    return y / x
+print(my_sum(a , b))
+print(my_subtract(a , b))
+print(my_multiply(a , b))
+print(my_divide(a , b))
