7851b9427dd1e994762a0b047b29f19c0c4d0332
Hossein2468 <hosseinbaziepic@gmail.com>
2022-11-12 21:11:37 +0330

    added 7 lines but not worked

--- problems/P024/P024-wikipedia.py
+++ problems/P024/P024-wikipedia.py
@@ -1,1 +1,7 @@
 a = str(input("Enter the link address: "))
+
+import urllib 
+from bs4 import BeautifulSoup
+b = urllib.urlopen(a) 
+soup = BeautifulSoup(b.content , "html.parser") 
+print(soup.prettify)
