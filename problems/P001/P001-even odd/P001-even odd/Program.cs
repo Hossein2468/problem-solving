533c9131a902e22fdf973987f784c430ab6b94bd
Hossein2468 <hosseinbaziepic@gmail.com>
2023-06-28 13:28:57 +0330

    Added P001 with csharp

---
+++ problems/P001/P001-even odd/P001-even odd/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P001_even_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.Write("number ");
+                Console.Write(a);
+                Console.Write(" is even.");
+            }
+            else
+            {
+                Console.Write("number ");
+                Console.Write(a);
+                Console.Write(" is odd. ");
+            }
+        }
+    }
+}
