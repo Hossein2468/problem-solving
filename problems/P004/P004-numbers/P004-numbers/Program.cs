685f080b0d9b225e542ad5bd5c94c9d79039f114
Hossein2468 <hosseinbaziepic@gmail.com.com>
2023-07-02 17:41:03 +0330

    Added P005 with Csharp

---
+++ problems/P004/P004-numbers/P004-numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace P004_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
