10dbff876476aa33664be6fd2831c7c3a42216bf
Hossein2468 <hosseinbaziepic@gmail.com.com>
2023-07-02 17:21:16 +0330

    Added P003 with Csharp

---
+++ problems/P003/P003-days of year/P003-days of year/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P003_days_of_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first year: ");
+            int a = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Enter the second year: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"there is {Math.Abs((a * 365) - (b * 365))} days between {a} and {b} . ");
+        }
+    }
+}
