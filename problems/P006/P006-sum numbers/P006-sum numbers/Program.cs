585eff74cb6de54ab3a52d7da4aabe8661d2683a
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-04 13:39:01 +0330

    Added P006 with Csharp

---
+++ problems/P006/P006-sum numbers/P006-sum numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P006_sum_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            int c = 0;
+            while (b <= a)
+            {
+                c = c + b;
+                b++;
+            }
+            Console.WriteLine(c); 
+        }
+    }
+}
