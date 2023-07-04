04c857fb3659948ed21a8a37dcacc67f4dc4b58b
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-04 13:52:42 +0330

    Added P008 with Csharp

---
+++ problems/P008/P008-factorial/P008-factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P008_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 1;
+            for (int c = 1; c <= a; c++)
+            { b = b * c; }
+            Console.WriteLine(b);
+        }
+    }
+}
