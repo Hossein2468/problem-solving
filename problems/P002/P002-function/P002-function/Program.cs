75ce8a67d36479de86a6ecebb42e88005d881ef7
Hossein2468 <hosseinbaziepic@gmail.com.com>
2023-07-04 12:10:24 +0330

    added P002 with Csharp

--- problems/P002/P002-function/P002-function/Program.cs
+++ problems/P002/P002-function/P002-function/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int b = Convert.ToInt32(Console.ReadLine()); 
+            static int sum(int x , int y) { return x + y; } 
+            static int subtract(int x , int y) { return x - y; }
+            static int multiply(int x , int y) { return x * y; }
+            static double divide(double x , double y) { return x / y; }
+            Console.WriteLine($"sum of numbers is {sum(a, b)}");
+            Console.WriteLine($"subtract of numbers is {subtract(a, b)}");
+            Console.WriteLine($"multiply of numbers is {multiply(a, b)}");
+            Console.WriteLine($"divide of numbers is {divide(a, b)}");
         }
     }
 }
