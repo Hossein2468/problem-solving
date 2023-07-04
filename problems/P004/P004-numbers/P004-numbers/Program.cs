68a1a67aac977a39afd08cc232f20fb9fa1727ac
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-04 12:32:40 +0330

    Added P004 with Csharp

--- problems/P004/P004-numbers/P004-numbers/Program.cs
+++ problems/P004/P004-numbers/P004-numbers/Program.cs
@@ -6,6 +6,20 @@
         {
             Console.WriteLine("Enter the number: ");
             int a = Convert.ToInt32(Console.ReadLine());
+            Dictionary<int, string> numbers = new Dictionary<int, string>()
+            {
+                {1 , "One" } ,
+                {2 , "Two" } ,
+                {3 , "Three" } ,
+                {4 , "Four" } ,
+                {5 , "Five" } ,
+                {6 , "Six" } ,
+                {7 , "Seven" } ,
+                {8 , "Eight" } ,
+                {9 , "Nine" } ,
+                {10 , "Ten" }
+            };
+            Console.WriteLine(numbers[a]);
         }
     }
 }
