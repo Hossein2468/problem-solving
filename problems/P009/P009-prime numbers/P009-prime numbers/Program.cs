c316e85490c0658ef42872d9bb55668b21325b40
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-06 14:03:06 +0330

    Added P009 with Csharp

---
+++ problems/P009/P009-prime numbers/P009-prime numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P009_prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            static bool isprime(int m)
+            {
+                bool p = true;
+                for (int x = 2; x < m; x++)
+                {
+                    var o = m % x == 0;
+                    if (o == true)
+                    {
+                        p = false;
+                        break;
+                    }
+                }
+                return p;
+            }
+            for (int m = 2; m <= n; m++)
+            {
+                if (isprime(m))
+                { Console.WriteLine(m); }
+            }
+        }
+    }
+}
