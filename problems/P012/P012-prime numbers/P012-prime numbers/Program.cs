b6908bbfc34bcc62d8948aaf9c2aaef9a1fd2e21
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-06 19:04:21 +0330

    Added P012 with Csharp

---
+++ problems/P012/P012-prime numbers/P012-prime numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P012_prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            static bool is_prime(int m)
+            { bool p = true; 
+            for (int x = 2; x < m; x++)
+                { var o = m % x == 0;
+                if (o == true)
+                    { p = false;
+                        break;
+                    }
+                }
+                return p;
+            }
+            int a = 0;
+            int u = 2; 
+            while (a <= n)
+            { if (is_prime(u) == true)
+                { a++;
+                    if (a == n)
+                    { Console.WriteLine(u); }
+                }
+                u++;
+            }
+        }
+    }
+}
