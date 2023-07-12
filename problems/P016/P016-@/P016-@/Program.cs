d684e8736304bb2cd968f6d88556ef6a9db9530e
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-12 12:55:26 +0330

    Added P016 with Csharp

---
+++ problems/P016/P016-@/P016-@/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P016__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            while (a >= 0)
+            {
+                int b = 0;
+                while(b < a)
+                {
+                    Console.Write("@");
+                    b++;
+                }
+                Console.WriteLine();
+                a--;
+            }
+        }
+    }
+}
