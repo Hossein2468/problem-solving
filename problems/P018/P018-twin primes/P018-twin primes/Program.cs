37d16fe3e2ba88f48ea2fc96214a92023fdb5be8
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-12 13:37:31 +0330

    Added P018 with Csharp

---
+++ problems/P018/P018-twin primes/P018-twin primes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P018_twin_primes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            static bool is_prime(int m)
+            {
+                bool p = true;
+                for (int x = 2; x < m; x++)
+                {
+                    var o = m % x == 0;
+                    if (o == true)
+                    {
+                        p = false;
+                        break;
+                    }
+                }
+                return p;
+            }
+            for (int m = 5; m <= n; m++)
+            {
+                if (is_prime(m) == true)
+                {
+                    if (is_prime(m - 2) == true)
+                    {
+                        Console.Write(m - 2);
+                        Console.Write(", ");
+                        Console.Write(m);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
