4951a491d4e6deb536e935f4e3c331c1a9370bf6
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-12 13:21:41 +0330

    Added P017 with Csharp

---
+++ problems/P017/P017-0 and +/P017-0 and +/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P017_0_and__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = n;
+            while (n >= 0)
+            {
+                int a = 0;
+                while (a < n)
+                {
+                    Console.Write("+");
+                    a++;
+                }
+                int h = i - n;
+                while (h > 0)
+                {
+                    Console.Write("0");
+                    h--;
+                }
+                Console.WriteLine();
+                n--;
+            }
+        }
+    }
+}
