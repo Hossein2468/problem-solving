b98d0c2f6c7d4c532d573cf054e7cb3cfe2bf16d
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-12 12:44:12 +0330

    Added P015 with Csharp

---
+++ problems/P015/P015-sumlist/P015-sumlist/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P015_sumlist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of numbers: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            for (int a = 0; a < n; a++)
+            {
+                var m = Convert.ToInt32(Console.ReadLine());
+                x = x + m;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
