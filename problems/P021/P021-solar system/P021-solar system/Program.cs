90ad55f49c58d8c9646ae1804647b503d1c5ea5b
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-13 14:06:47 +0330

    Added P021 with Csharp

---
+++ problems/P021/P021-solar system/P021-solar system/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P021_solar_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> solar_system = new Dictionary<string, int>()
+            {
+                { "Mercry", 58 },
+                { "Venus", 120 },
+                { "Earth", 180 },
+                { "Mars", 250 },
+                { "Jupiter", 777 },
+                { "Saturn", 1430 },
+                { "Uranus", 2880 },
+                { "Neptune", 4500 }
+            };
+            foreach (KeyValuePair<string, int> a in solar_system)
+            {
+                int b = a.Value / 58;
+                string c = new string('-', b);
+                Console.WriteLine($"sun {c} {a.Key}");
+            }
+        }
+    }
+}
