431a5dc176b3b06ae16ce71b24f77a18dc07721a
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-13 13:37:06 +0330

    Added P020 with Csharp

---
+++ problems/P020/P020-numbers/P020-numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P020_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Dictionary<int, string> numbers = new Dictionary<int, string>()
+            {
+                {1 , "One" } ,
+                {2 , "Two" } ,
+                {3 , "Three" } ,
+                {4 , "Four" } ,
+                {5 , "Five" } ,
+                {6 , "Six" } ,
+                {7 , "Seven" } ,
+                {8 , "Eight" } ,
+                {9 , "Nine" } ,
+                {10 , "Ten" }
+            };
+            Console.WriteLine(numbers[a]);
+        }
+    }
+}
