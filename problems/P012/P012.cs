2f60f7e638f4733569308d8ad141008e14a74fb7
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-17 15:56:36 +0330

    repair P012

---
+++ problems/P012/P012.cs
@@ -0,0 +1,42 @@
+﻿namespace Solution
+{
+    internal class P012
+    {
+        public void Solution()
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Prime(n));
+        }
+        public int Prime(int n)
+        {
+            static bool is_prime(int m)
+            {
+                bool p = true;
+                for (int x = 2; x < m; x++)
+                {
+                    var o = m % x == 0;
+                    if (o == true)
+                    {
+                        p = false;
+                        break;
+                    }
+                }
+                return p;
+            }
+            int a = 0;
+            int u = 2;
+            while (a <= n)
+            {
+                if (is_prime(u) == true)
+                {
+                    a++;
+                    if (a == n)
+                    { return u; }
+                }
+                u++;
+            }
+            return 0;
+        }
+    }
+}
