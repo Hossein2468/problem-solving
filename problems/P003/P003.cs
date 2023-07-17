35ad0c3dc754d690b2c4cac4b1a934f8898355d2
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-17 14:49:41 +0330

    commit

---
+++ problems/P003/P003.cs
@@ -0,0 +1,18 @@
+﻿namespace Solution
+{
+    internal class P003
+    {
+        public void Solution()
+        {
+            Console.WriteLine("Enter the first year: ");
+            int a = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Enter the second year: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Days_Of_Year(a, b));
+        }
+        public string Days_Of_Year(int a, int b)
+        {
+            return $"there is {Math.Abs((a * 365) - (b * 365))} days between {a} and {b} . ";
+        }
+    }
+}
