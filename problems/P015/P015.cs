0cd6b0144106f3dc872fa4ed9a26eb4ea971145b
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-17 16:08:12 +0330

    repair P015

--- problems/P015/P015.cs
+++ problems/P015/P015.cs
@@ -1,18 +1,22 @@
-﻿namespace P015_sumlist
+﻿namespace Solution
 {
-    internal class Program
+    internal class P015
     {
-        static void Main(string[] args)
+        public void Solution()
         {
             Console.WriteLine("Enter the number of numbers: ");
             int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Sum_List(n));
+        }
+        public int Sum_List(int n)
+        {
             int x = 0;
             for (int a = 0; a < n; a++)
             {
                 var m = Convert.ToInt32(Console.ReadLine());
                 x = x + m;
             }
-            Console.WriteLine(x);
+            return x;
         }
     }
 }
