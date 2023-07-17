abc710eba45f346c54bbf521834936b017fc5396
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-17 15:31:20 +0330

    repair P005

---
+++ problems/P005/P005.cs
@@ -0,0 +1,25 @@
+﻿namespace Solution
+{
+    internal class P005
+    {
+        public void Solution()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operator: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Enter the second number: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Operator(a, b, c));
+        }
+        public int Operator(int a, string b, int c)
+        {
+            int d = 0;
+            if (b == "+") { d = a + c; }
+            if (b == "-") { d = a - c; }
+            if (b == "*") { d = a * c; }
+            if (b == "/") { d = a / c; }
+            return d;
+        }
+    }
+}
