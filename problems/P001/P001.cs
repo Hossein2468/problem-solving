a7bfe97d0cf25aa6af2b5a684ef825ece308c20c
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-17 13:32:32 +0330

    repair P001

---
+++ problems/P001/P001.cs
@@ -0,0 +1,24 @@
+﻿namespace Solution
+{
+    internal class P001
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Even_Odd(a));
+            
+        }
+        public string Even_Odd(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return $"number {a} is even. ";
+            }
+            else
+            {
+                return $"number {a} is odd. ";
+            }
+        }
+    }
+}
