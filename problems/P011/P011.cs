7f1bb7a910dd9074f130c5f379204277f7f4f1bb
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-17 15:53:20 +0330

    repair P011

---
+++ problems/P011/P011.cs
@@ -0,0 +1,25 @@
+﻿namespace Solution
+{
+    internal class P011
+    {
+        public void Solution()
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fibonacci(n));
+        }
+        public int Fibonacci(int n)
+        {
+            int a = 1; int b = 1;
+            Console.WriteLine(a); Console.WriteLine(b);
+            while (a <= n || b <= n)
+            {
+                a = a + b;
+                if (a <= n) { return a; }
+                b = b + a;
+                if (b <= n) { return b; }
+            }
+            return 0;
+        }
+    }
+}
