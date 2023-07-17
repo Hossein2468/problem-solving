d3cd918f416271e17f7b57a201a75e5d3eaa7b04
Hossein2468 <hosseinbaziepic@gmail.com>
2023-07-17 15:40:01 +0330

    repair P007

---
+++ problems/P007/P007.cs
@@ -0,0 +1,18 @@
+﻿namespace Solution
+{
+    internal class P007
+    {
+        public void Solution()
+        {
+            Console.WriteLine("Enter the number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Number(a));
+        }
+        public int Number(int a)
+        {
+            for (int b = 0; b <= a; b = b + 5)
+            { if (b > 1) { return b; } }
+            return 0;
+        }
+    }
+}
